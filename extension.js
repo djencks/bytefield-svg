78f5cb9b71c03b4ad6ba4fdd220e7d94476e7d14
David Jencks <djencks@apache.org>
2020-03-02 09:18:52 -0800

    set up to use plugin to install extension in Antora

--- extension.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-const generate = require('./lib.js')
-const extension = require('asciidoctor-generic-svg-extension')
-
-module.exports.register = (registry) => extension.register(registry, 'bytefield', generate)
-
